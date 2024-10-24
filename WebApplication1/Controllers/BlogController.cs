431e0a9fb5f0d67499b0bded4445075024a39588
Nguyen khanh toan <nguyenkhanhtoan588@gmail.com>
2024-10-24 20:47:53 +0700

    ngày 13/10 làm bài th8 hiển thị chi tiêt blog, số comment

---
+++ WebApplication1/Controllers/BlogController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly HarmicContext _context;
+        public BlogController(HarmicContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/blog/{alias}-{id}.html")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.TblBlogs == null)
+            {
+                return NotFound();
+            }
+            var blog = await _context.TblBlogs.FirstOrDefaultAsync(m => m.BlogId == id);
+            if(blog == null)
+            {
+                return NotFound();
+            }
+            ViewBag.blogComment = _context.TblBlogComments.Where(i => i.BlogId == id).ToList();
+            return View(blog);
+        }
+    }
+
+}
